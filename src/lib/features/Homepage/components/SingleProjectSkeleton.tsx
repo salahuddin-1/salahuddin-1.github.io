cbef1fffff780dc5241424765aafac8b7784f31e
Salahuddin Shaikh <salahuddinshaikh16@gmail.com>
2024-03-11 16:28:54 +0530

    shadow added on our work section

--- src/lib/features/Homepage/components/SingleProjectSkeleton.tsx
+++ src/lib/features/Homepage/components/SingleProjectSkeleton.tsx
@@ -81,7 +81,7 @@ const SingleProjectSkeleton = ({ project }: Props) => {
     <Box
       onClick={onClickWork}
       cursor="pointer"
-      boxShadow="0px 2px 10px rgba(0, 0, 0, 0.1)"
+      boxShadow="0px 2px 10px rgba(255, 255, 255, 0.25)"
       borderRadius={10}
       borderWidth={0.1}
       borderColor="#4F4F4F"
