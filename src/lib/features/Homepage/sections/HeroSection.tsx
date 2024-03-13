f104da53683d19934ea6331b60b4da4010a52f63
Salahuddin Shaikh <salahuddinshaikh16@gmail.com>
2024-03-13 16:30:55 +0530

    Profile Hero section done

--- src/lib/features/Homepage/sections/HeroSection.tsx
+++ src/lib/features/Homepage/sections/HeroSection.tsx
@@ -1,4 +1,4 @@
-import { Box, Image, useBreakpointValue } from "@chakra-ui/react";
+import { Box, Image, Text, VStack, useBreakpointValue } from "@chakra-ui/react";
 import AppPageHeading from "../../../ui/components/AppPageHeading";
 import AppMaterialButton from "../../../ui/components/AppMaterialButton";
 import { useHomepageViewModel } from "src/lib/providers/HomepageViewModelProvider";
@@ -8,6 +8,7 @@ import { RoutePaths } from "src/lib/navigation/route_paths";
 import AnimateOnLoad from "@components/AnimateOnLoad";
 import LottieHeroAnimation from "../components/LottieHeroAnimation";
 import ticTacToeimage from "@assets/tic_tac_toe_table.png";
+import profilePhotoHero from "@assets/profile_photo_hero.jpeg";
 
 const HeroSection = () => {
   const viewModel = useHomepageViewModel();
@@ -32,7 +33,7 @@ const HeroSection = () => {
     >
       <Box
         justifyContent="center"
-        flex="1"
+        flex="3"
         display="flex"
         flexDirection="column"
       >
@@ -72,15 +73,116 @@ const HeroSection = () => {
       <Box height="75px" />
       <Box width="75px" />
 
-      <AnimateOnLoad duration={1} delay={0.75} translateY={100}>
-        <Box flex="1" display="flex" justifyContent="center" backgroundColor="">
-          {/* <Image src={ticTacToeimage}></Image> */}
-          {/* <HeroImage /> */}
+      {/* Profile Image and Designation */}
+      <Box
+        position="relative"
+        flex="2"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        backgroundColor=""
+      >
+        {/* <AnimateOnLoad duration={1} delay={0.75} translateY={100}> */}
+        <Image src={ticTacToeimage}></Image>
+        {/* </AnimateOnLoad> */}
 
-          <LottieHeroAnimation width={lottieWidth} />
-        </Box>
-      </AnimateOnLoad>
+        <_ProfileImage />
+
+        {/* <Box
+          borderRadius="50%"
+          top="30px"
+          right="30px"
+          position="absolute"
+          boxShadow="-2px 5px 40px 0px rgba(250,250,250,0.25)"
+          border="1px solid white"
+          // boxSize="25%"
+          // aspectRatio={1 / 1}
+          height="10vw"
+          width="10vw"
+          bg="red"
+        >
+          <img src={profilePhotoHero}></img>
+        </Box> */}
+      </Box>
     </Box>
+  );
+};
+
+const _ProfileImage = () => {
+  const size = useBreakpointValue({
+    base: "30%",
+    sm: "25%",
+    md: "45%",
+    lg: "35%",
+    "2xl": "20%",
+  });
+
+  const fontSizeTitle = useBreakpointValue({
+    base: "20px",
+    sm: "35px",
+    md: "20px",
+    lg: "35px",
+    "2xl": "45px",
+  });
+
+  const fontSizeSubtitle = useBreakpointValue({
+    base: "10px",
+    sm: "15px",
+    md: "10px",
+    lg: "15px",
+    "2xl": "20px",
+  });
+
+  return (
+    <VStack
+      alignItems={{
+        base: "center",
+        md: "end",
+      }}
+      top={{
+        md: "45px",
+      }}
+      right={{
+        md: "35px",
+      }}
+      position="absolute"
+    >
+      <Image
+        src={profilePhotoHero}
+        alt="Profile Photo Hero"
+        borderRadius="full"
+        height={size}
+        width={size}
+        aspectRatio={1 / 1}
+        objectFit="cover"
+        boxShadow="-2px 5px 40px 0px rgba(250,250,250,0.3)"
+        border="1px solid white"
+      />
+
+      <Box height="20px"></Box>
+
+      <Text
+        textAlign={{
+          base: "center",
+          md: "end",
+        }}
+        fontSize={fontSizeTitle}
+        fontWeight="400"
+      >
+        Salahuddin Shaikh
+      </Text>
+
+      <Text
+        textAlign={{
+          base: "center",
+          md: "end",
+        }}
+        fontSize={fontSizeSubtitle}
+        fontWeight="400"
+      >
+        Software Engineer
+      </Text>
+    </VStack>
   );
 };
 
