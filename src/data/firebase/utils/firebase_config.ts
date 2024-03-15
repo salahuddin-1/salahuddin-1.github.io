43b9c123a22c4a8d6592098b6a3727da3165b6d6
Salahuddin Shaikh <salahuddinshaikh16@gmail.com>
2024-03-16 02:53:00 +0530

    Added google API key in .env

--- src/data/firebase/utils/firebase_config.ts
+++ src/data/firebase/utils/firebase_config.ts
@@ -7,7 +7,7 @@ import { getFirestore } from "firebase/firestore/lite";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD1Pc4HmqWDOogeuyHnKImJNv23pi14k0g",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "salahuddin-website-portfolio.firebaseapp.com",
   projectId: "salahuddin-website-portfolio",
   storageBucket: "salahuddin-website-portfolio.appspot.com",
