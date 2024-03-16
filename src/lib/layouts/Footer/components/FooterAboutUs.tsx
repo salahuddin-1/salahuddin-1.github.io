a3335f6822025b2fed8ee4d034583bbb825ef86d
Salahuddin Shaikh <salahuddinshaikh16@gmail.com>
2024-03-16 20:43:21 +0530

    About us altered

--- src/lib/layouts/Footer/components/FooterAboutUs.tsx
+++ src/lib/layouts/Footer/components/FooterAboutUs.tsx
@@ -20,20 +20,10 @@ const FooterAboutUs = () => {
         expertise to every project. During my tenure at NeoSOFT Technologies
         until January 09, 2024, I led the development of the "Capital Bank of
         Jordan" app, overseeing its inception, architecture setup, and the
-        creation of custom reusable widgets. Let’s understand what do I mean by
-        a Software System, for example, you're a business owner who sells skin
-        products and wants to take your business online through an E-commerce
-        store. I'll help you build an end-user Mobile Application for Android
-        and iOS, a Responsive Web Application for both mobile and desktop
-        browsers, and an Admin Portal for managing these software solutions. The
-        Admin Portal will include features like user tracking and management,
-        review management, notifications, coupon code creation, a forced update
-        toggle, a kill switch for emergency shutdowns, and much more. All
-        applications will be secured with strong encryption techniques and
-        algorithms. I’ll work with you on every step, understanding your
-        problem, simplifying the problem, brainstorming on a solution,
-        collecting your functional and non-functional requirements, collecting
-        the digital assets required, understanding your pain points,
+        creation of custom reusable widgets. I’ll work with you on every step,
+        understanding your problem, simplifying the problem, brainstorming on a
+        solution, collecting your functional and non-functional requirements,
+        collecting the digital assets required, understanding your pain points,
         understanding most important quality attributes all with in accordance
         to your budget.
       </FooterText>
