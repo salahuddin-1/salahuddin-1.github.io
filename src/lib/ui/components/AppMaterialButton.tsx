56f88f080f60f459d8c5550393247da5970dbcba
Salahuddin Shaikh <salahuddinshaikh16@gmail.com>
2024-03-25 04:33:24 +0530

    Button animation in progress

--- src/lib/ui/components/AppMaterialButton.tsx
+++ src/lib/ui/components/AppMaterialButton.tsx
@@ -1,6 +1,7 @@
 import { Box, useTheme } from "@chakra-ui/react";
 import { CustomTheme } from "../../../theme";
 import { AppColor } from "src/domain/constants/AppColor";
+import { motion } from "framer-motion";
 
 interface Props {
   children: string | JSX.Element;
@@ -36,6 +37,49 @@ const AppMaterialButton = (props: Props) => {
       {props.children}
     </Box>
   );
+
+  // return (
+  //   <motion.div
+  //     style={{
+  //       backgroundColor: theme.colors.secondary,
+  //       width: props.width === undefined ? "fit-content" : props.width,
+  //       borderRadius: "10px",
+  //       color: AppColor.BLACK,
+  //     }}
+  //     whileHover={{
+  //       backgroundColor: theme.colors.primary,
+  //       transition: { duration: 0.1 },
+  //       borderRadius: "10px",
+  //       border: `0.1px solid ${AppColor.LIGHT_GRAY_100}`,
+  //       color: AppColor.WHITE,
+  //       // scale: 1.09,
+  //       // translateY: 20,
+
+  //       // transition: { duration: 0.5 },
+  //     }}
+  //   >
+  //     <Box
+  //       disabled={props.isDisabled}
+  //       type={props.type}
+  //       onClick={() => props.onClick()}
+  //       width={props.width === undefined ? "fit-content" : props.width}
+  //       height={props.height}
+  //       paddingX="25px"
+  //       paddingY="10px"
+  //       // backgroundColor={theme.colors.secondary}
+  //       // border={`0.1px solid ${AppColor.LIGHT_GRAY_100}`}
+  //       borderRadius="10px"
+  //       // textColor={theme.colors.primary}
+  //       boxSizing="border-box"
+  //       fontWeight={400}
+  //       fontSize="13px"
+  //       as="button"
+  //       cursor={props.isDisabled ? "not-allowed" : "pointer"}
+  //     >
+  //       {props.children}
+  //     </Box>
+  //   </motion.div>
+  // );
 };
 
 export default AppMaterialButton;
