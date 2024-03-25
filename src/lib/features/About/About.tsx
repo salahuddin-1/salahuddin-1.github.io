174ee6cd5b90ce7902b2ecb3527a82c3b82a89c9
Salahuddin Shaikh <salahuddinshaikh16@gmail.com>
2024-03-26 04:16:07 +0530

    What I do added

--- src/lib/features/About/About.tsx
+++ src/lib/features/About/About.tsx
@@ -2,8 +2,11 @@ import { SectionSpacing } from "@components/SectionSpacing";
 import BasePage from "src/lib/base/BasePage";
 import { useAboutViewModel } from "src/lib/providers/AboutViewModelProvider";
 import AboutHeroSection from "./sections/AboutHeroSection";
-import { Box, Image, Text } from "@chakra-ui/react";
+import { Box, Image, Text, VStack } from "@chakra-ui/react";
 import profilePhotoHero from "@assets/profile_photo_5.jpeg";
+import AppPageSubheading from "@components/AppPageSubheading";
+import AppPageHeading from "@components/AppPageHeading";
+import AppSectionHeading from "@components/AppSectionHeading";
 
 const About = () => {
   const viewModel = useAboutViewModel();
@@ -37,14 +40,26 @@ const About = () => {
           alignItems="start"
           // alignItems="start"
         >
-          <Text>
-            Hi, I'm Salahuddin Shaikh, the founder of an agency that helps small
-            to medium-sized businesses go online by creating custom Software
-            Systems using the latest technologies. I have 5 years of experience
-            in developing cross-platform mobile applications with Flutter. I've
-            built apps in many domains and recently served as a team lead at
-            Capital Bank of Jordan for almost 3 years.
-          </Text>
+          <VStack alignItems="start">
+            <Text>
+              Hi, I'm Salahuddin Shaikh, the founder of an agency that helps
+              small to medium-sized businesses go online by creating custom
+              Software Systems using the latest technologies. I have 5 years of
+              experience in developing cross-platform mobile applications with
+              Flutter.
+            </Text>
+
+            <Box height="30px" />
+
+            <AppSectionHeading>What I do?</AppSectionHeading>
+            <Box height="10px" />
+
+            <Text>
+              I help people solve their business problems with software. I
+              assist traditional offline businesses in transitioning online by
+              developing customized Software Systems tailored to their needs.
+            </Text>
+          </VStack>
         </Box>
 
         <Box height="60px" />
